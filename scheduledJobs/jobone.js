5a0a85e7542151ccf01b359c19d920dde0905f6a
Javier Garcia <javier.garcia@edenicgames.com>
2016-07-05 08:40:27 +0200

    Test pull from dashboard

--- scheduledJobs/jobone.js
+++ scheduledJobs/jobone.js
@@ -1,1 +1,2 @@
 log.info("jobone.js"); 
+log.info("Get matcheds query: { $and: [ {\"s\":\"playing\"}, {$or: [ { u1.uid: { $in: [\"56d7e78ce4b0bbd9366232d9\",\"5714d429e4b0852ee51f855e\",\"5714d4c8e4b0d3076056e20c\",\"5714d522e4b0852ee51f8667\",\"56e964bce4b027e49c1e4c03\"]} } , { u2.uid: { $in: [\"56d7e78ce4b0bbd9366232d9\",\"5714d429e4b0852ee51f855e\",\"5714d4c8e4b0d3076056e20c\",\"5714d522e4b0852ee51f8667\",\"56e964bce4b027e49c1e4c03\"]} } ] } ] }");
